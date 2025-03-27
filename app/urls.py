d0a19d417aa5f13f246f98de22539803d7748796
gaspatt <matheusgaspar29.if@gmail.com>
2025-03-27 16:07:28 -0300

    .

--- app/urls.py
+++ app/urls.py
@@ -8,6 +8,7 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 
 from core.views import UserViewSet
+from core.views import CategoriaViewSet # nova linha
 
 router = DefaultRouter()
 
@@ -30,3 +31,7 @@ urlpatterns = [
     # API
     path('api/', include(router.urls)),
 ]
+
+
+router.register(r"categorias", CategoriaViewSet) # nova linha
+router.register(r"users", UserViewSet, basename="users")
